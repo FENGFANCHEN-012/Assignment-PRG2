dce3795e1698380920206e34489f2cf385d36fe7
FENGFANCHEN-012 <Chensail@outlook.com>
2025-01-26 18:00:28 +0800

    function commit

--- Assignment PRG2/Program.cs
+++ Assignment PRG2/Program.cs
@@ -19,7 +19,7 @@ namespace Assignment_PRG2
         {
 
 
-            
+
 
             Console.WriteLine("=============================================\r\nWelcome to Changi Airport Terminal 5\r\n=============================================\r\n1. List All Flights\r\n2. List Boarding Gates\r\n3. Assign a Boarding Gate to a Flight\r\n4. Create Flight\r\n5. Display Airline Flights\r\n6. Modify Flight Details\r\n7. Display Flight Schedule\r\n0. Exit\r\nPlease select your option:\r\n1");
             string answer = Console.ReadLine()!;
@@ -35,66 +35,23 @@ namespace Assignment_PRG2
             List<string> airdata = new List<string>(File.ReadLines(airline));
             List<string> gatedata = new List<string>(File.ReadLines(gate));
 
-            foreach (string i in airdata)   /// load airlines file
+
+            void LoadAirLine()
             {
-                if (airdata.IndexOf(i) == 0)
+                foreach (string i in airdata)   /// load airlines file
                 {
-                    continue;
-                }
-                List<string> data = new List<string>(i.Split(","));
-                airdic[data[1]] = new AirLine(data[0], data[1], flightdic);
-
-            }
-             
-            foreach (string i in flightdata)  /// load flight file
-            {
-                if (flightdata.IndexOf(i) == 0)
-                {
-                    continue;
-                }
-                List<string> data = new List<string>(i.Split(","));
-
-                if (data[4] == "")
-
-                {
-                    Console.WriteLine(data[4]);
-                    flightdic[data[0]] = new NORMFlight(data[0], data[1], data[2], Convert.ToDateTime(data[3]), data[4]);
-                }
-                else if (data[4] == "CFFT")
-                {
-                    flightdic[data[0]] = new CFFTFlight(data[0], data[1], data[2], Convert.ToDateTime(data[3]), data[4]);
-                }
-                else if (data[4] == "LWTT")
-                {
-                    flightdic[data[0]] = new LWTTFlight(data[0], data[1], data[2], Convert.ToDateTime(data[3]), data[4]);
-                }
-                else if (data[4] == "DDJB")
-                {
-                    flightdic[data[0]] = new DDJBFlight(data[0], data[1], data[2], Convert.ToDateTime(data[3]), data[4]);
-                }
-
-
-            }
-
-            // feature 3
-            if (answer == "1")
-            {
-                Console.WriteLine($"{"Flight Number",-30}{"AirLine Name",-30}{"Origin",-27}{"destination",-30}{"Expected Arrival",-15}");
-                foreach (var i in flightdic)
-                {
-
-                    foreach (var w in airdic)
+                    if (airdata.IndexOf(i) == 0)
                     {
-                        if (w.Value.Code == i.Value.FlightNumber.Substring(0, 2))
-                        {
-                            Console.WriteLine($"{i.Value.FlightNumber,-30}{w.Value.Name,-30}{i.Value.Origin,-27}{i.Value.Destination,-30}{i.Value.ExpectedTime,-15}");
-                        }
-
+                        continue;
                     }
+                    List<string> data = new List<string>(i.Split(","));
+                    airdic[data[1]] = new AirLine(data[0], data[1], flightdic);
+
                 }
             }
-            /// create the boardgate object
-          
+            void LoadGate() {
+
+
                 foreach (string i in gatedata.Skip(1))
                 {
 
@@ -104,43 +61,107 @@ namespace Assignment_PRG2
 
                 }
 
-
-            /// Testing execution of files
-            Console.WriteLine($"Loaded airlines: {airdic.Count}");
-            Console.WriteLine($"Loaded flights: {flightdic.Count}");
-            Console.WriteLine($"Loaded boarding gates: {gatedic.Count}");
-
-            //feature 4
-            if (answer == "2") {
-                Console.WriteLine($"{"Gate NAME",-10}{"DDJB",-10}{"CFFT",-10}{"LWTT",-10}");
-                foreach (var i in gatedic.Values)
-                {
-                    Console.WriteLine($"{i.GetName,-10}{i.SupportsDDJB,-10}{i.SupportsCFFT,-10}{i.SupportsLWTT,-10}");
-                }
-
-
-
-
-
-
             }
-            if (answer == "3") { }
-            foreach (var w in gatedic)
+
+            void LoadFlight()
             {
-
-
-                foreach (var i in flightdic)
+                foreach (string i in flightdata)  /// load flight file
                 {
-
-                    if (i.Value.Status == w.)
+                    if (flightdata.IndexOf(i) == 0)
                     {
+                        continue;
+                    }
+                    List<string> data = new List<string>(i.Split(","));
+
+                    if (data[4] == "")
+
+                    {
+                        Console.WriteLine(data[4]);
+                        flightdic[data[0]] = new NORMFlight(data[0], data[1], data[2], Convert.ToDateTime(data[3]), data[4]);
+                    }
+                    else if (data[4] == "CFFT")
+                    {
+                        flightdic[data[0]] = new CFFTFlight(data[0], data[1], data[2], Convert.ToDateTime(data[3]), data[4]);
+                    }
+                    else if (data[4] == "LWTT")
+                    {
+                        flightdic[data[0]] = new LWTTFlight(data[0], data[1], data[2], Convert.ToDateTime(data[3]), data[4]);
+                    }
+                    else if (data[4] == "DDJB")
+                    {
+                        flightdic[data[0]] = new DDJBFlight(data[0], data[1], data[2], Convert.ToDateTime(data[3]), data[4]);
+                    }
+
+
+                }
+            }
+
+
+            // feature 3
+            void Command() {
+                while (true) {
+                    if (answer == "1")
+                    {
+                        Console.WriteLine($"{"Flight Number",-30}{"AirLine Name",-30}{"Origin",-27}{"destination",-30}{"Expected Arrival",-15}");
+                        foreach (var i in flightdic)
+                        {
+
+                            foreach (var w in airdic)
+                            {
+                                if (w.Value.Code == i.Value.FlightNumber.Substring(0, 2))
+                                {
+                                    Console.WriteLine($"{i.Value.FlightNumber,-30}{w.Value.Name,-30}{i.Value.Origin,-27}{i.Value.Destination,-30}{i.Value.ExpectedTime,-15}");
+                                }
+
+                            }
+                        }
+                    }
+                    /// create the boardgate object
+
+                 
+
+
+                    /// Testing execution of files
+
+
+                    //feature 4
+                    if (answer == "2") {
+                        Console.WriteLine($"{"Gate NAME",-10}{"DDJB",-10}{"CFFT",-10}{"LWTT",-10}");
+                        foreach (var i in gatedic.Values)
+                        {
+                            Console.WriteLine($"{i.GetName,-10}{i.SupportsDDJB,-10}{i.SupportsCFFT,-10}{i.SupportsLWTT,-10}");
+                        }
+
+
+
+
+
 
                     }
-                } }
+                    if (answer == "3") { }
+                    foreach (var w in gatedic)
+                    {
+
+
+                        foreach (var i in flightdic)
+                        {
+
+                            if (i.Value.Status == w.)
+                            {
+
+                            }
+                        } }
+
+                }
+
+            }
+
+            LoadAirLine();
+            LoadFlight();
+            LoadGate();
+            Command();
+
             Console.WriteLine($"Loaded airlines: {airdic.Count}");
             Console.WriteLine($"Loaded flights: {flightdic.Count}");
-            foreach (var i in gatedic)
-            {
-                Console.WriteLine(i.Value.Name);
-            }
-        } } }}
+        } } }
+            
